e191d4de9f2f71b234376ab707224c518e83733a
Tormod Flesjø <tormod.flesjo@gmail.com>
2019-03-25 12:22:28 +0100

    instructor code

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -1,61 +1,107 @@
- function turnHoursToMinutes(mov){
-  var newMovies = mov.map(function(movie){
-    var duration = movie.duration;
-    var indexhours = duration.indexOf('h');
-    var hours = 0;
-    var minutes = 0;
-    if(indexhours != -1){
-      hours = duration.slice(0,indexhours);
-      hours = hours * 60;
+clearArray = (str) => {
+    var clear = str.split('h');
+    clear = clear.join(" ").replace('min', '');
+    clear = clear.split(" ");
+    return clear;
+}
+
+turnHoursToMinutes = (movies) => {
+ return movies.map((movie) => {
+    const duration = clearArray(movie.duration);
+    var newDuration = 0;
+    if(duration.length != 1){
+    const hour = duration[0]*60;
+    const min = parseInt(duration[2]);
+        if(duration.length === 2){
+             newDuration = hour;
+        }else{
+            newDuration = hour+min;
+        }
+    }else{
+        newDuration =  parseInt(duration[0]);
+    }  
+    return Object.assign({...movie, duration: newDuration});
+});
+}
+ratesAverage = movies => {
+    if(movies.length !=0){
+ const moviesLen = movies.length;
+ const sumRate =  movies.reduce((sum, movie) => {
+     if(movie.rate === ""){
+        return sum + 0;
+     }else{
+        return sum + parseFloat(movie.rate);
+     }
+     }
+      ,0);
+      const avgRate = sumRate/moviesLen;
+    return parseFloat(avgRate.toFixed(2));
     }
-    var indexminutes=duration.indexOf('m');
-    if(indexminutes != -1 && indexhours != -1)
-      minutes = duration.slice(indexhours+2,indexminutes);
-    if(indexminutes != -1 && indexhours == -1)
-      minutes = duration.slice(0,indexminutes);
-    var copy = Object.assign({}, movie);
-    var totalminutes = parseInt(hours) + parseInt(minutes);
-    copy.duration = totalminutes;
-    
-    return copy;
-  });
-  
-    return newMovies;
 }
 
-function ratesAverage(mov){
-  var sum = 0;
-  
-  mov.forEach(function(movie){
-    if(isNaN(parseFloat(movie.rate)))
-      sum+=0;
-    else
-      sum+=parseFloat(movie.rate);
-  });
-  return sum/mov.length;
-  
-}
-function dramaMoviesRate(mov){
-  var newMoviesDrama = mov
-                      .filter(movie => (movie.genre
-                      .filter(genre => genre === 'Drama')).length>0);
-  if(newMoviesDrama.length > 0)
-    return parseFloat(ratesAverage(newMoviesDrama).toFixed(2));
-}
-
-// How many movies did STEVEN SPIELBERG
-function howManyMovies (movies){
-  if (movies.length === 0) return undefined;
-    var spielbergMovies = movies.filter(function(el){
-      return el.genre.includes('Drama') && el.director.includes('Steven Spielberg')
-    });
-    
-    console.log("length", spielbergMovies.length)
-    console.log("this one",spielbergMovies)
-    console.log(`Steven Spielberg directed ${spielbergMovies.length} drama movies!`)
-    return `Steven Spielberg directed ${spielbergMovies.length} drama movies!`
+dramaMoviesRate = movies => { 
+    const drama = movies.filter((movie) => {
+          if(movie.genre.includes('Drama')){
+              return movie;
+          }
+      });
+    return ratesAverage(drama);  
   }
-console.log(howManyMovies(movies));
+
+orderByDuration = array => {
+    return array.sort((a, b) => {
+        if(a.duration - b.duration === 0){
+            return  (a.title > b.title) || (a.title === b.title) -1;
+        }else{
+            return a.duration - b.duration;
+        }
+    });
+}
+howManyMovies = array => {
+    if(array != 0){
+        const stevenMovies = array.filter(movie => {
+            if(movie.genre.includes('Drama'))
+                if(movie.director.includes('Spielberg'))
+                    return movie;         
+        }).length;
+        if(stevenMovies != 0){
+            return `Steven Spielberg directed ${stevenMovies} drama movies!`;
+        }else{
+            return 'Steven Spielberg directed 0 drama movies!';  
+        } 
+    }     
+} 
+orderAlphabetically = array => {
+    let twentyMovies = [];
+    if(array != 0){
+        const ordered = array.sort((a,b) => {
+            return  (a.title > b.title) || (a.title === b.title) -1;
+        }); 
+        if(ordered.length > 20){
+            for(let i = 0; i < 20; i++){
+                twentyMovies.push(ordered[i].title);
+            }
+            return twentyMovies;
+        }else{
+           return ordered.map(movie => movie.title);
+        }
+        
+    }else{
+        return twentyMovies;
+    }
+} 
 
 
-
+bestYearAvg = movies => {
+    if(movies != 0){
+    const years = movies.map(movie => movie.year).sort((a,b) => a-b);
+    const yearsAvg = years.map(year => {
+        return {
+            year: year,
+            avgRate: ratesAverage(movies.filter(movie => movie.year === year))
+        }
+    });
+    yearsAvg.sort((a,b) => b.avgRate - a.avgRate);
+    return `The best year was ${yearsAvg[0].year} with an average rate of ${yearsAvg[0].avgRate}`;
+    }
+}
